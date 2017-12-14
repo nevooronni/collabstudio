0ae3d7c7e77ad7281ff5791e9b714665ca402002
Neville Oronni <nevooronni@gmail.com>
2017-12-14 11:56:32 +0300

    added git hub repository

--- capstone/collabstudio/urls.py
+++ capstone/collabstudio/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url,include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings#IMPORT SETTINGS
+from django.conf.urls.static import static#USE STATIC FILES
 from . import views 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
